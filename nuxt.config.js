317929c0ed8ed99974eea4caef08f3c22e195b6d
Thomas X. Wu <thomas.wu@kp.org>
2018-03-19 21:52:27 -0700

    init repo

---
+++ nuxt.config.js
@@ -0,0 +1,41 @@
+module.exports = {
+  /*
+  ** Headers of the page
+  */
+  head: {
+    title: 'hello-nuxt',
+    meta: [
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { hid: 'description', name: 'description', content: 'Nuxt.js project' }
+    ],
+    link: [
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+    ]
+  },
+  // plugins: ['~plugins/buefy.js'],
+  /*
+  ** Customize the progress bar color
+  */
+  loading: { color: '#ED0000' },
+  /*
+  ** Build configuration
+  */
+  build: {
+
+    vendor: ['Buefy'],
+    /*
+    ** Run ESLint on save
+    */
+    extend (config, { isDev, isClient }) {
+      if (isDev && isClient) {
+        config.module.rules.push({
+          enforce: 'pre',
+          test: /\.(js|vue)$/,
+          loader: 'eslint-loader',
+          exclude: /(node_modules)/
+        })
+      }
+    }
+  }
+}
